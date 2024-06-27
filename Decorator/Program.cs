29df0189592671e4793fdf189e6061d35c2e9341
Hasan <65603504+hasanyurdakul@users.noreply.github.com>
2024-06-27 20:00:17 +0300

    added Decorator Pattern

---
+++ Decorator/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+ * Sınıfın yapısında değişikliğe gitmeden belirli bir nesneye veya bazı nesnelere yeni ek özellikler ya da fonksiyonları dinamik olarak eklemek istediğimizde kullanılır.
+ * 
+ * Buradaki önemli ayrıntı, yeni özelliğin sınıfa değil, sınıftan türeyen belirli bir nesneye(instance'a) eklenmesidir.
+ * Yani bu sınıftan türetilen diğer nesneler,bu ek özellikten veya fonksiyondan etkilenmezler.
+ */
